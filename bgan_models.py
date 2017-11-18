3c7adbf3cb8b01d004e390efe572bc9ff02029e8
Alex <kaw293@cornell.edu>
2017-11-18 16:01:44 -0500

    alex cleverhans path

--- bgan_models.py
+++ bgan_models.py
@@ -8,7 +8,7 @@ from bgan_util import AttributeDict
 
 #### Bayesian DCGAN
 import sys
-sys.path.insert(0, '/Users/mattwallingford/Documents/cleverhans')
+sys.path.insert(0, '/home/alex/cleverhans')
 from cleverhans.attacks import FastGradientMethod
 from cleverhans.utils_tf import model_train, model_eval, model_loss
 from cleverhans.model import Model
