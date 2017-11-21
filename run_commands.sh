9a8fbc993f9d6a73132c807c8d74c065cc2f914f
Ubuntu <ubuntu@ip-172-31-25-50.ec2.internal>
2017-11-21 00:46:42 +0000

    fixing cleverhans path for ubuntu

--- run_commands.sh
+++ run_commands.sh
@@ -1,3 +1,3 @@
 #python bayesian_gan_hmc.py --out_dir 'results' --semi_supervised --save_weights --save_samples --adv_test --train_iter 800 --data_path '' --n_save 10 --num_mcmc 4
 #python bayesian_gan_hmc.py --out_dir 'results' --semi_supervised --save_weights --save_samples --adv_test --train_iter 1000 --data_path '' --n_save 100  --num_mcmc 2  --numz 10 
-python bayesian_gan_hmc.py --out_dir 'results' --semi_supervised --save_weights --save_samples --adv_test --train_iter 20 --data_path '' --save_chkpt 10 --n_save 10  --num_mcmc 1  --numz 1 
+python bayesian_gan_hmc.py --out_dir 'results' --semi_supervised --save_weights --save_samples --adv_test --train_iter 1000 --data_path '' --save_chkpt 100 --n_save 100 --num_mcmc 2  --numz 10
